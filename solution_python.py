83b314e9336d4c5586fcea8334feaf339d1cf11d
Rohit Chaudhry <66793185+rohitchaudhry362@users.noreply.github.com>
2020-07-10 14:22:29 -0400

    Update solution_python.py

--- solution_python.py
+++ solution_python.py
@@ -20,38 +20,41 @@ class EventSourcer():
         undoAction.append('-')
 
     def undo(self):
-    	if len(undoAction) >0:
-	        if undoAction[-1] == '+':
-	            self.value -= undoValue[-1]
-	            temp = undoValue.pop()
-	            redoValue.append(temp)
-	            temp= undoAction.pop()
-	            redoAction.append(temp)
+        if len(undoAction) >0:
+            if undoAction[-1] == '+':
+                self.value -= undoValue[-1]
+                temp = undoValue.pop()
+                redoValue.append(temp)
+                temp= undoAction.pop()
+                redoAction.append(temp)
 
             
-        	elif undoAction[-1] == '-':
-        		self.value += undoValue[-1]
-        		temp = undoValue.pop()
-        		redoValue.append(temp)
-        		temp = undoAction.pop()
-        		redoAction.append(temp)
+            elif undoAction[-1] == '-':
+                self.value += undoValue[-1]
+                temp = undoValue.pop()
+                redoValue.append(temp)
+                temp = undoAction.pop()
+                redoAction.append(temp)
+        else:
+            print("undo cannot be done")
 
     def redo(self):
-    	if len(redoAction) >0:
-	        if redoAction[-1] == '+':
-	            self.value += redoValue[-1]
-	            temp = redoAction.pop()
-	            undoAction.append(temp)
-	            temp = redoValue.pop()
-	            undoValue.append(temp)
+        if len(redoAction) >0:
+            if redoAction[-1] == '+':
+                self.value += redoValue[-1]
+                temp = redoAction.pop()
+                undoAction.append(temp)
+                temp = redoValue.pop()
+                undoValue.append(temp)
 
-	        elif redoAction[-1] == '-':
-	            self.value -= redoValue[-1]
-	            temp = redoAction.pop()
-	            undoAction.append(temp)
-	            temp = redoValue.pop()
-	            undoValue.append(temp)
-        
+            elif redoAction[-1] == '-':
+                self.value -= redoValue[-1]
+                temp = redoAction.pop()
+                undoAction.append(temp)
+                temp = redoValue.pop()
+                undoValue.append(temp)
+        else:
+            print("redo cannot be done")
 
     def bulk_undo(self, steps: int):
         for i in range(0,steps):
@@ -60,7 +63,3 @@ class EventSourcer():
     def bulk_redo(self, steps: int):
         for i in range(0,steps):
             self.redo()
-
-
-
-
